cf71dd371dd1ebde88b25e027777eba2ad44ec83
Robert James <95536550+robertjames9@users.noreply.github.com>
2023-03-12 00:14:08 +0800

    Update and rename teslabotz.sol to tesla.sol

--- tesla.sol
+++ tesla.sol
@@ -1,4 +1,4 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -6,24 +6,15 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Tesla is Ownable {
+    contract Tesla is Ownable {
     using SafeERC20 for IERC20;
     using Counters for Counters.Counter;
-    IERC20 public token;
-    Counters.Counter private dailyLimit;
 
-    uint[] public commRate = [70, 40, 30, 20]; //referrals commission rate
-    uint[] public sameRankCommRate = [0, 5, 5, 10]; //same rank commission rate
-    uint public commFeeRate = 180; //total commission should be payout
-    uint public levelStep = 4; //total level of membership
-    uint private companyFee = 150; //company earning
-    uint public poolRate = 20; //for sharing prize pool
-    uint public marketingFee = 150; //for marketing purpose
-    uint public available;
+    IERC20 public token;
     uint public price;
     uint public minUnits = 1;
-    uint public maxUnits = 2;
-    uint public _dailyLimit = 1000;
+    uint public maxUnits = 1;
+    uint public dailyLimit = 1000;
     uint public totalDeposit;
     uint public totalBonus;
     uint public totalAllocated;
@@ -33,11 +24,21 @@ contract Tesla is Ownable {
     uint public payMultiplier = 2;
     uint public totalUsers;
 
-    address private companyWallet = 0x7b421084368661260bE1FAD06660940EAd41A50b;
-    address private marketingWallet = 0x7b421084368661260bE1FAD06660940EAd41A50b;
-    address private poolWallet = 0x7b421084368661260bE1FAD06660940EAd41A50b;
+    Counters.Counter private _dailyLimit;
 
-    bool started = false;
+    uint[] private commRate = [30, 60, 90, 120, 150]; //group commission rate
+    uint private directCommRate = 100; //direct sponsor commission
+    uint private commFeeRate = 150; //total group commission should be payout
+    uint private companyFee = 220; //company earning
+    uint private poolRate = 30; //for sharing prize pool
+    uint private levelStep = 15; //total max number to achieve next rank
+    uint private MAX_LEVEL = 5;
+    uint private available;
+
+    address private companyWallet = 0x7132aDC062d1a02bB13Aa650a5475252955fe373;
+    address private poolWallet = 0x7132aDC062d1a02bB13Aa650a5475252955fe373;
+
+    bool private started = false;
 
     struct Deposit {
         address account;
@@ -57,8 +58,10 @@ contract Tesla is Ownable {
         uint totalDeposit;
         uint totalAllocated;
         uint totalWithdrawn;
+        uint level;
         bool disableDeposit;
         bool activate;
+        bool autoReinvest;
         uint totalBonus;
         uint directBonus;
     }
@@ -73,15 +76,15 @@ contract Tesla is Ownable {
         uint lvl5;
     }
 
-    Deposit[] public deposits;
-    mapping(uint => User) public users;
-    mapping(uint => Level) public levels;
-    mapping(uint => uint) public checkpoints;
-    mapping(address => uint) public userids;
+    Deposit[] private deposits;
+    mapping(address => uint) private userids;
+    mapping(uint => User) private users;
+    mapping(uint => Level) private levels;
+    mapping(uint => uint) private checkpoints;
 
     event Commission(uint value);
     event UserMsg(uint userid, string msg, uint value);
-    
+
     constructor(IERC20 _token, uint _price) {
         token = _token;
         price = _price;
@@ -90,60 +93,61 @@ contract Tesla is Ownable {
     receive() external payable {}
 
     function withdraw() external onlyOwner {
-        if(address(this).balance > 0) {
+        if (address(this).balance > 0) {
             payable(companyWallet).transfer(address(this).balance);
         }
-        if(token.balanceOf(address(this)) > 0) {
+        if (token.balanceOf(address(this)) > 0) {
             token.safeTransfer(companyWallet, token.balanceOf(address(this)));
         }
     }
 
-     function invest(address referer, uint units) external {
+    function invest(address referer, uint units) external {
         require(started, "Investment program haven't start!");
-        require(units >= minUnits, "Less than Min Units");
-        require(units <= maxUnits, "Over than Max Units");
-        require(dailyLimit.current() <= _dailyLimit, "Over Daily Limit");
+        require(units >= minUnits && units <= maxUnits, "Invalid units");
+        require(_dailyLimit.current() <= dailyLimit, "Over Daily Limit");
         
-        dailyLimit.increment();
+        _dailyLimit.increment();
         processDeposit(referer, units);
-        payReferral(referer, units);
+        payDirect(referer, units);
+        payGroup(referer, units);
         payQueue();
     }
 
     function processDeposit(address referer, uint units) private {
         uint userid = userids[msg.sender];
         if (userid == 0) {
-            totalUsers += 1;
+            totalUsers++;
             userid = totalUsers;
             userids[msg.sender] = userid;
             checkpoints[userid] = block.timestamp;
             emit UserMsg(userid, "Joined", 0);
         }
+
         User storage user = users[userid];
-        if (user.account == address(0)) {
-            user.account = msg.sender;
-        }
-        require(user.disableDeposit != true, "Pending Withdraws");
+        require(!user.disableDeposit, "Pending Claims");
         user.disableDeposit = true;
         user.activate = true;
-        if (user.referer == address(0)) {
-            if (users[userids[referer]].deposits.length > 0 && referer != msg.sender) {
-                user.referer = referer;
-                users[userids[referer]].partners.push(userid);
-                processLevelUpdate(referer, msg.sender);
-            }
+        user.autoReinvest = true;
+
+        if (user.referer == address(0) && users[userids[referer]].deposits.length > 0 && referer != msg.sender) {
+            user.referer = referer;
+            users[userids[referer]].partners.push(userid);
+            processLevelUpdate(referer, msg.sender);
         }
 
-        require(units > 0);
-        require(units <= maxUnits, "Over than Max Units");
         uint value = units * price;
         token.safeTransferFrom(msg.sender, address(this), value);
         totalDeposit += value;
 
-        Deposit memory deposit;
-        deposit.amount = value;
-        deposit.account = msg.sender;
-        deposit.checkpoint = block.timestamp;
+        Deposit memory deposit = Deposit({
+            account: msg.sender,
+            amount: value,
+            payout: 0,
+            allocated: 0,
+            bonus: 0,
+            paid: false,
+            checkpoint: block.timestamp
+        });
 
         emit UserMsg(userids[msg.sender], "Deposit", value);
 
@@ -153,73 +157,94 @@ contract Tesla is Ownable {
         available += value;
     }
 
-    function payReferral(address referer, uint units) private returns (uint){
+    function payDirect(address referer, uint units) private {
         uint value = price * units;
-        uint currTotalCommission = value * commFeeRate / 1000;
-        uint remainingCommission = currTotalCommission;
-        uint totalRefOut;
-        uint prevUplineLevel = 0;
-        bool sameRankClaimed = false;
- 
+        uint directCommission = value * directCommRate / 1000;
         address upline = referer;
+        uint uplineId = userids[upline];
+        performTransfer(upline, uplineId, directCommission);
+        users[uplineId].directBonus += directCommission;
+        available -= directCommission;
+    }
 
-        for(uint i = 0; i < 4; i++) {
-            User storage user = users[userids[referer]];
-            uint uplineId = userids[upline];
-            uint currUplineLevel = levels[uplineId].level;
-            User storage currUplineUser = users[uplineId];
+    function payGroup(address referer, uint units) private {
+        uint value = price * units;
+        uint remainingCommission = value * commFeeRate / 1000;
+        uint prevUplineLevel;
+        uint totalRefOut;
+        bool sameRankClaimed;
 
-            if (uplineId == 0 || upline == address(0) || currUplineLevel < prevUplineLevel || user.activate == false) break;
-            
-            uint commission;
+        for (uint i = 0; i < 5; i++) {
+            uint currUplineId = userids[referer];
+            uint currUplineLevel = levels[currUplineId].level;
 
-            if(currUplineLevel > prevUplineLevel) {
+            if (currUplineId == 0 || referer == address(0) || currUplineLevel < prevUplineLevel || !users[currUplineId].activate) {
+                break;
+            }
+
+            uint commission = 0;
+            uint numSameRank = getNumSameRankUsers(currUplineLevel, referer);
+
+            if (currUplineLevel > prevUplineLevel) {
                 uint accruedRate = getAccruedReferralRate(prevUplineLevel, currUplineLevel);
-                if(accruedRate > 0) {
+                if (accruedRate > 0) {
                     commission = value * accruedRate / 1000;
-                    performTransfer(upline, uplineId, commission);
-                    totalRefOut = totalRefOut + commission;
-                    remainingCommission = remainingCommission - commission;
-                    if (i == 0)
-                        users[uplineId].directBonus += commission;
+                    if (numSameRank > 1) {
+                        commission /= numSameRank;
+                    }
+                    performTransfer(referer, currUplineId, commission);
+                    totalRefOut += commission;
+                    remainingCommission -= commission;
+                    sameRankClaimed = false;
                 }
-                sameRankClaimed = false;
-            }
-            else if(currUplineLevel == prevUplineLevel && !sameRankClaimed && currUplineLevel > 0) {
-                uint sameRankRate = sameRankCommRate[currUplineLevel - 1];
+            } else if (currUplineLevel == prevUplineLevel && !sameRankClaimed && currUplineLevel > 0) {
+                uint sameRankRate = commRate[currUplineLevel - 1];
                 if (sameRankRate > 0) {
-                    commission = value * sameRankCommRate[i] / 1000;
-                    performTransfer(upline, uplineId, commission);
-                    totalRefOut = totalRefOut + commission;
-                    remainingCommission = remainingCommission - commission;
-                    if (i == 0)
-                        currUplineUser.directBonus += commission;
+                    commission = value * sameRankRate / 1000;
+                    if (numSameRank > 1) {
+                        commission /= numSameRank;
+                    }
+                    performTransfer(referer, currUplineId, commission);
+                    totalRefOut += commission;
+                    remainingCommission -= commission;
                     sameRankClaimed = true;
+                    i--;
                 }
-
-                i--;
             }
-            upline = currUplineUser.referer;
+
             prevUplineLevel = currUplineLevel;
+            referer = users[currUplineId].referer;
         }
 
         if (remainingCommission > 0) {
             token.safeTransfer(companyWallet, remainingCommission);
+            totalCommission += remainingCommission;
+            available -= remainingCommission;
         }
 
         totalBonus += totalRefOut;
-        totalCommission += currTotalCommission;
-        available -= currTotalCommission;
         uint companyOut = value * companyFee / 1000;
         token.safeTransfer(companyWallet, companyOut);
-        uint marketingOut = value * marketingFee / 1000;
-        token.safeTransfer(marketingWallet, marketingOut);
         uint poolOut = value * poolRate / 1000;
         token.safeTransfer(poolWallet, poolOut);
-        uint commi = companyOut + marketingOut + poolOut;
-        emit Commission(commi);
-        available -= commi;
-        return commi;
+        uint cost = companyOut + poolOut;
+        emit Commission(cost);
+        available -= cost;
+    }
+
+    function getNumSameRankUsers(uint rank, address upline) private view returns (uint) {
+        uint numSameRank = 0;
+        address currUpline = upline;
+        while (currUpline != address(0)) {
+            uint currUplineId = userids[currUpline];
+            uint currUplineLevel = levels[currUplineId].level;
+            if (currUplineLevel != rank) {
+                break;
+            }
+            numSameRank++;
+            currUpline = users[currUplineId].referer;
+        }
+        return numSameRank;
     }
 
     function performTransfer(address referer, uint uplineId, uint amount) private {
@@ -228,11 +253,13 @@ contract Tesla is Ownable {
     }
 
     function getAccruedReferralRate(uint prevLevel, uint currLevel) private view returns (uint) {
-        uint rateSum = 0; 
-        if(prevLevel > currLevel)
+        if (prevLevel >= currLevel || currLevel > MAX_LEVEL) {
             return 0;
-        for(uint i = prevLevel; i < currLevel; i++) {
-            rateSum = rateSum + commRate[i];
+        }
+
+        uint rateSum;
+        for (uint i = prevLevel; i < currLevel; i++) {
+            rateSum += commRate[i];
         }
         return rateSum;
     }
@@ -279,7 +306,7 @@ contract Tesla is Ownable {
                 emit UserMsg(userids[referer], "LevelUp", 5);
                 processLevelUpdate(user.referer, referer);
             }
-        } 
+        }
     }
 
     function payQueue() private {
@@ -342,37 +369,57 @@ contract Tesla is Ownable {
         }
     }
 
-    function claim() external {
+    function reInvest() private {
         User storage user = users[userids[msg.sender]];
-        require(user.deposits.length > 0, "User has No Deposits");
-        uint topay;
-        uint i;
-        for (i = 0; i < user.deposits.length; i++) {
-            Deposit storage deposit = deposits[user.deposits[i]];
-            if (deposit.allocated > deposit.payout) {
-                uint onepay = deposit.allocated - deposit.payout;
-                deposit.payout += onepay;
-                user.totalWithdrawn += onepay;
-                totalWithdrawn += onepay;
-                topay += onepay;
+        require(user.deposits.length > 0, "User has no deposits");
+        require(user.autoReinvest == true, "Auto invest is not available!");
 
-            if (deposit.allocated == deposit.payout){
-                user.activate = false;
+        for (uint i = 0; i < user.deposits.length; i++) {
+            Deposit storage deposit = deposits[user.deposits[i]];
+            if (deposit.allocated == deposit.amount * 2) {
+                uint reinvestAmount = price * maxUnits;
+                deposit.allocated = deposit.amount + reinvestAmount;
+                available -= reinvestAmount;
+                user.totalDeposit += reinvestAmount;
+                user.disableDeposit = true;
+                emit UserMsg(userids[msg.sender], "Reinvest", reinvestAmount);
+                processDeposit(msg.sender, reinvestAmount);
             }
         }
     }
-        user.disableDeposit = false;
-        require(topay > 0, "Nothing to claim");
-        token.safeTransfer(msg.sender, topay);
-        emit UserMsg(userids[msg.sender], "Claim", topay);
-}
 
-    function resetCount() external onlyOwner {
-        dailyLimit.reset();
+    function claim() external {
+        User storage user = users[userids[msg.sender]];
+        require(user.deposits.length > 0, "User has no deposits");
+
+        uint totalPayout;
+        for (uint i = 0; i < user.deposits.length; i++) {
+            Deposit storage deposit = deposits[user.deposits[i]];
+            if (deposit.allocated > deposit.payout) {
+                uint payoutAmount = deposit.allocated - deposit.payout;
+                deposit.payout += payoutAmount;
+                user.totalWithdrawn += payoutAmount;
+                totalWithdrawn += payoutAmount;
+                totalPayout += payoutAmount;
+            }
+
+            if (deposit.allocated == deposit.payout) {
+                user.activate = false;
+            }
+        }
+
+        require(totalPayout > 0, "No payout available");
+        token.safeTransfer(msg.sender, totalPayout);
+        emit UserMsg(userids[msg.sender], "Claim", totalPayout);
+        user.autoReinvest = false;
     }
 
-    function setDailyLimit(uint dayLimit) external onlyOwner {
-        _dailyLimit = dayLimit;
+    function resetCount() external onlyOwner {
+        _dailyLimit.reset();
+    }
+
+    function setDailyLimit(uint _dayLimit) external onlyOwner {
+        dailyLimit = _dayLimit;
     }
 
     function setLevel(uint userid, uint level) external onlyOwner {   
@@ -405,10 +452,6 @@ contract Tesla is Ownable {
 
     function setCommRate(uint256[] memory rates) external onlyOwner {
         commRate = rates;
-    }
-
-    function setSRankRate(uint256[] memory rates) external onlyOwner {
-        sameRankCommRate = rates;
     }
 
     function setPayMultiplier(uint multiplier) external onlyOwner {
@@ -461,20 +504,22 @@ contract Tesla is Ownable {
         );
     }
 
-    function userInfo(uint userId) public view returns (address, uint, address, uint, uint, uint, uint, uint, uint) {
+    function userInfo(uint userId) public view returns (address, uint, address, uint, uint, uint, uint, uint, uint, uint, bool) {
         require(userId > 0 && userId <= totalUsers, "Invalid user ID");
         User memory user = users[userId];
 
         return (
             user.referer,
             userids[user.referer],
-            user.account, 
+            user.account,
+            user.level, 
             user.partners.length,
             user.totalDeposit,
             user.totalAllocated,
             user.totalWithdrawn,
             user.totalBonus,
-            user.directBonus
+            user.directBonus,
+            user.activate
         );
     }
 }
